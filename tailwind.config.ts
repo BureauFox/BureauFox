769ab3559267d47bf75140fe8158ac1ea4a41304
BureauFox <team.bureaufox@gmail.com>
2025-12-17 00:42:03 +0500

    fix css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,23 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary-rgb) / <alpha-value>)',
+        darkmode: 'rgb(var(--color-darkmode-rgb) / <alpha-value>)',
+        midnight_text: 'rgb(var(--color-midnight-text-rgb) / <alpha-value>)',
+        'midnight-text': 'rgb(var(--color-midnight-text-rgb) / <alpha-value>)',
+        purple: 'rgb(var(--color-purple-rgb) / <alpha-value>)',
+        grey: 'rgb(var(--color-grey-rgb) / <alpha-value>)',
+        border: 'rgb(var(--color-border-rgb) / <alpha-value>)',
+        'deep-slate': 'rgb(var(--color-deep-slate-rgb) / <alpha-value>)',
+        'deep_slate': 'rgb(var(--color-deep-slate-rgb) / <alpha-value>)',
+        error: 'rgb(var(--color-error-rgb) / <alpha-value>)',
+        warning: 'rgb(var(--color-warning-rgb) / <alpha-value>)',
+      },
+    },
+  },
 };
 
 export default config;
